b224c4b727a6ccebcab20449700d72358d5ebe88
GDAsim <gamedevelopementasim@gmail.com>
2024-07-08 11:19:53 +0800

    add polygon to mesh

---
+++ Assets/ProceduralMesh/PolygonMesh/Example/PolygonMesh_Example.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+[RequireComponent( typeof( PolygonCollider2D ), typeof( MeshFilter ), typeof( MeshRenderer ) )]
+[ExecuteInEditMode]
+public class PolyWall : MonoBehaviour 
+{
+	Vector2[] prevPoints;
+    MeshFilter meshFilter;
+    PolygonCollider2D polygonCollider2D;
+
+    void Awake()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+        polygonCollider2D = GetComponent<PolygonCollider2D>();
+    }
+
+    void Update()
+    {
+		if (prevPoints != polygonCollider2D.points)
+		{
+            meshFilter.mesh = PolygonToMesh();
+            prevPoints = polygonCollider2D.points;
+		}
+	}
+
+    Mesh PolygonToMesh()
+	{
+		var points = polygonCollider2D.points;
+		Vector3[] vertices = new Vector3[points.Length];
+		for(int i = 0; i < vertices.Length; i++)
+        {
+			vertices[i] = points[i];
+		}
+
+        int[] triangles = TriangulatePoints.Triangulate(points);
+
+        Mesh mesh = new();
+        mesh.vertices = vertices;
+		mesh.triangles = triangles;
+		return mesh;
+	}
+}
