80f7b31861148b346949e72ceab3e574e290eab5
Khiew Jian Bin <khiewbin@gmail.com>
2026-05-07 21:48:25 +0800

    Update Cube_Example.cs

--- Assets/ProceduralGeneration/Mesh/Basic/Cube/Example/Cube_Example.cs
+++ Assets/ProceduralGeneration/Mesh/Basic/Cube/Example/Cube_Example.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 
+[ExecuteInEditMode]
+[RequireComponent (typeof(MeshFilter), typeof(MeshRenderer))]
 public class Cube_Example : MonoBehaviour
 {
     [Header("Mesh")]
@@ -8,70 +10,80 @@ public class Cube_Example : MonoBehaviour
     [Header("Settings")]
     [SerializeField] Vector3 size = Vector3.one;
 
-    GameObject meshGO;
     Mesh mesh;
 
-    void Start()
+    void Update()
     {
-        meshGO = new($"Cube Mesh");
-        mesh = new Mesh();
-        mesh.hideFlags = HideFlags.DontSave;
-        meshGO.transform.parent = transform;
-        meshGO.AddComponent<MeshFilter>();
-        meshGO.AddComponent<MeshRenderer>();
-        meshGO.GetComponent<Renderer>().material = meshMaterial;
-        meshGO.GetComponent<MeshFilter>().mesh = mesh;
+        if (!gameObject.GetComponent<MeshFilter>())
+        {
+            gameObject.AddComponent<MeshFilter>();
+            gameObject.GetComponent<MeshFilter>().mesh = mesh;
+        }
+        
+        if (!gameObject.GetComponent<MeshRenderer>())
+        {
+            gameObject.AddComponent<MeshRenderer>();
+            gameObject.GetComponent<MeshRenderer>().material = meshMaterial;
+        }
+
+        
     }
     void OnValidate()
     {
-        if (meshGO == null)
-        {
-            meshGO = new($"Cube Mesh");
-            mesh = new Mesh();
-            mesh.hideFlags = HideFlags.DontSave;
-            meshGO.transform.parent = transform;
-            meshGO.AddComponent<MeshFilter>();
-            meshGO.AddComponent<MeshRenderer>();
-            meshGO.GetComponent<Renderer>().material = meshMaterial;
-            meshGO.GetComponent<MeshFilter>().mesh = mesh;
-        }
+        mesh = new Mesh();
+        mesh.hideFlags = HideFlags.DontSave;
+        gameObject.GetComponent<MeshFilter>().mesh = mesh;
+        gameObject.GetComponent<MeshRenderer>().material = meshMaterial;
 
         var F_TL = new Vector3(-1, 1, -1);
-        var F_TR = new Vector3(1, 1, -1);
         var F_BL = new Vector3(-1, -1, -1);
         var F_BR = new Vector3(1, -1, -1);
+        var F_TR = new Vector3(1, 1, -1);
         var B_TL = new Vector3(-1, 1, 1);
-        var B_TR = new Vector3(1, 1, 1);
         var B_BL = new Vector3(-1, -1, 1);
         var B_BR = new Vector3(1, -1, 1);
+        var B_TR = new Vector3(1, 1, 1);
 
-        F_TL = Vector3.Scale(F_TL, size);
-        F_TR = Vector3.Scale(F_TR, size);
-        F_BL = Vector3.Scale(F_BL, size);
-        F_BR = Vector3.Scale(F_BR, size);
-        B_TL = Vector3.Scale(B_TL, size);
-        B_TR = Vector3.Scale(B_TR, size);
-        B_BL = Vector3.Scale(B_BL, size);
-        B_BR = Vector3.Scale(B_BR, size);
-
-        var triangles = new int[]
-        {
-            0, 2, 1,   0, 3, 2,     // front
-            4, 6, 5,   4, 7, 6,     // back
-            8, 10, 9,  8, 11, 10,   // top
-            12, 14, 13, 12, 15, 14, // bottom
-            16, 18, 17, 16, 19, 18, // right
-            20, 22, 21, 20, 23, 22  // left
-        };
+        var s = size / 2f;
+        F_TL = Vector3.Scale(F_TL, s);
+        F_BL = Vector3.Scale(F_BL, s);
+        F_BR = Vector3.Scale(F_BR, s);
+        F_TR = Vector3.Scale(F_TR, s);
+        B_TL = Vector3.Scale(B_TL, s);
+        B_BL = Vector3.Scale(B_BL, s);
+        B_BR = Vector3.Scale(B_BR, s);
+        B_TR = Vector3.Scale(B_TR, s);
 
         var vertices = new Vector3[8]
         {
-            F_TL,F_TR,F_BL,F_BR,
-            B_TL,B_TR,B_BL,B_BR
+            F_TL,F_BL,F_BR,F_TR,
+            B_TL,B_BL,B_BR,B_TR
         };
+        var triangles = new int[]
+        {
+            0, 2, 1,   0, 3, 2,     // front
+            4, 3, 0,   4, 7, 3,     // top
+            4, 1, 5,   4, 0, 1,     // left
+            3, 6, 2,   3, 7, 6,     // right
+            1, 6, 5,   1, 2, 6,     // bottom
+            7, 5, 6,   7, 4, 5      // back
+        };
+        var uvs = new Vector2[8]
+        {
+            new Vector2(0, 1), // F_TL
+            new Vector2(0, 0), // F_BL
+            new Vector2(1, 0), // F_BR
+            new Vector2(1, 1), // F_TR
+            new Vector2(0, 1), // B_TL
+            new Vector2(0, 0), // B_BL
+            new Vector2(1, 0), // B_BR
+            new Vector2(1, 1)  // B_TR
+        };
+
 
         mesh.vertices = vertices;
         mesh.triangles = triangles;
+        mesh.uv = uvs;
         mesh.RecalculateNormals();
         mesh.RecalculateBounds();
     }
